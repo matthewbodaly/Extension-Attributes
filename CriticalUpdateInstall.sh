0fb57076881a5552cdeb31dbc964c1207f46c048
Matthew <matthewbodaly@users.noreply.github.com>
2017-05-03 14:47:35 -0700

    Create CriticalUpdateInstall.sh

---
+++ CriticalUpdateInstall.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# Displays T if there is a critical software update available for install 
+status=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall)
+printf '<result>'
+if [ "$status" == "1" ]; then
+  printf 'T'
+else
+  printf 'F'
+fi
+printf '</result>'
+#ea_display_name    CriticalUpdateInstall
